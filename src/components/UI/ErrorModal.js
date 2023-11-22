03909201dbf8b7485db9402859863f04233b3d7b
Nice-Roz <115996172+Nice-Roz@users.noreply.github.com>
2023-11-22 03:17:33 -0800

    Add files via upload

---
+++ src/components/UI/ErrorModal.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import  ReactDom  from 'react-dom';
+import Card from './Card';
+import Button from './Button';
+import classes from './ErrorModal.module.css';
+import Wrapper from '../../Helpers/Wrapper';
+const BackDrop=(props)=>{
+  return  <div className={classes.backdrop} onClick={props.onConfirm} />;
+}
+const Overlay = (props)=>{
+  return(  <Card className={classes.modal}>
+    <header className={classes.header}>
+      <h2>{props.title}</h2>
+    </header>
+    <div className={classes.content}>
+      <p>{props.message}</p>
+    </div>
+    <footer className={classes.actions}>
+      <Button onClick={props.onConfirm}>Okay</Button>
+    </footer>
+  </Card>)
+}
+const ErrorModal = (props) => {
+  return (
+    <> 
+    {ReactDom.createPortal(<BackDrop onConfirm={props.onConfirm}/>,document.getElementById('backdrop'))}
+    {ReactDom.createPortal(<Overlay title={props.title} message={props.message} onConfirm={props.onConfirm}/>,document.getElementById('overlay'))}
+    </>
+  );
+};
+
+export default ErrorModal;
